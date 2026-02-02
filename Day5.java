7e8fdf9cff42778d00c12505d4c6cd675ae57332
Pawar Sushant Hanumant <sushantpawar1232@gmail.com>
2026-02-02 23:26:14 +0530

    GFG Problem: Reverse an Array

---
+++ Day5.java
@@ -0,0 +1,24 @@
+// GFG Problem: Reverse an Array
+
+import java.util.Arrays;
+
+public class Day5 {
+    public static void reverseArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Original array: " + Arrays.toString(arr));
+        reverseArray(arr);
+        System.out.println("Reversed array: " + Arrays.toString(arr));
+    }
+}
