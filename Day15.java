47b3a64e9ccf632799f123751ea0c78f78f0b984
Pawar Sushant Hanumant <sushantpawar1232@gmail.com>
2026-02-13 07:34:19 +0530

    GFG160 Day15 Problem: Smallest Positive Missing

--- Day15.java
+++ Day15.java
@@ -1,5 +1,27 @@
 // GFG160 Day15 Problem: Smallest Positive Missing
 
+import java.util.Arrays;
+
 public class Day15 {
-    
+    public int missingNumber(int[] arr) {
+        int missingNum = 1;
+        
+        Arrays.sort(arr);
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(missingNum == arr[i]) {
+                missingNum++;
+            }
+        }
+        
+        return missingNum;
+    }
+
+    public static void main(String[] args) { 
+        Day15 day15 = new Day15();
+         
+        int[] arr = {0, -10, 1, 3, -20}; 
+        int result = day15.missingNumber(arr); 
+        
+        System.out.println(result); }
 }
